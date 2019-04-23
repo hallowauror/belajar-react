f2670c69e9564a3186f49a68d2eadcdf5edef223
Ega Arya <ega0110@gmail.com>
2019-04-23 13:38:43 +0700

    React with material-ui

--- src/App.js
+++ src/App.js
@@ -1,83 +1,53 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Product from "./Product";
+import './bootstrap.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import Button from '@material-ui/core/Button';
+import Navbar from './PrimarySearchAppBar';
+import Card from './RecipeReviewCard';
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -7.7742906, lng: 110.374724 }}
+    >
+        {props.isMarkerShown && <Marker position={{ lat: -7.7742906, lng: 110.374724 }} />}
+    </GoogleMap>
+))
+
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            products: []
-        };
-    }
-
-    componentDidMount() {
-        let api_url = 'https://my-json-server.typicode.com/techtona/product_api2/products';
-        fetch(api_url)
-            .then(response => response.json())
-            .then(data => {
-                this.setState({products: data});
-            })
-            .catch(function (e) {
-                console.log(e);
-            });
-    }
-
-    hapusBaris = (key) => {
-        this.state.products.splice(key,1);
-        this.setState({products : this.state.products});
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-
-        let judul = this.refs.nama.value;
-        let harga = this.refs.harga.value;
-        let id = this.refs.id.value;
-
-        // console.log(nama,harga,id);
-        this.state.products.push({id,judul,harga});
-        this.setState({mahasiswa: this.state.mahasiswa});
-
-        this.refs.form.reset();
-    }
 
     render() {
         return (
-            <div className="row">
-                <div className="col l4">
-                    <form ref="form" onSubmit={this.handleSubmit}>
-                        <input type="text" ref="id" placeholder="ID Produk"/>
-                        <input type="text" ref="nama" placeholder="Nama Produk"/>
-                        <input type="text" ref="harga" placeholder="Harga"/>
-                        <button type="submit" className="btn waves-effect">Simpan</button>
-                    </form>
+            <div>
+                <Navbar>
+                    <br>
+                        <Card></Card>
+                    </br>
+                </Navbar>
+                <div className="text-center">
+                    <br></br>
+                    <h1>Google Map with REACT JS</h1>
+                    <hr></hr><br></br><br></br>
                 </div>
-                <div className="col l8">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>Nama Produk</th>
-                            <th>Harga</th>
-                            <th>Pilihan</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.products.map((data, key) => {
-                            return (
-                                <tr key={data.id}>
-                                    <td>{data.judul}</td>
-                                    <td>{data.harga}</td>
-                                    <td>
-                                        <button onClick={()=>this.hapusBaris(key)} className="waves-effect waves-teal btn-small red">Hapus</button>
-                                    </td>
-                                </tr>
-                            )
-                        })}
-                        </tbody>
-                    </table>
+                <br></br>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            MAPS
+            <div>
+                                <MyMapComponent
+                                    isMarkerShown
+                                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCp_difsopFyvuYa-RmUyZ_JIp66XF1ovQ&v=3.exp&libraries=geometry,drawing,places"
+                                    loadingElement={<div style={{ height: `100%` }} />}
+                                    containerElement={<div style={{ height: `400px` }} />}
+                                    mapElement={<div style={{ height: `100%` }} />}
+                                />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                
             </div>
         );
     }
