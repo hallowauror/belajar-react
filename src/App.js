482603086d13fa073b547e63928f78e8805e8db1
Ega Arya <ega0110@gmail.com>
2019-05-06 20:51:33 +0700

    Pertemuan SPA

--- src/App.js
+++ src/App.js
@@ -1,54 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
-import './bootstrap.css';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
-import Button from '@material-ui/core/Button';
-import Navbar from './PrimarySearchAppBar';
-import Card from './RecipeReviewCard';
-
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
-    <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -7.7742906, lng: 110.374724 }}
-    >
-        {props.isMarkerShown && <Marker position={{ lat: -7.7742906, lng: 110.374724 }} />}
-    </GoogleMap>
-))
-
+import Main from './component/Main';
+import { Link, BrowserRouter, Switch, Route } from 'react-router-dom';
+import Kontak from "./component/Kontak";
+import Profile from "./component/Profile";
+import Portofolio from "./component/Portofolio";
 
 class App extends Component {
-
     render() {
         return (
-            <div>
-                <Navbar>
-                    <br>
-                        <Card></Card>
-                    </br>
-                </Navbar>
-                <div className="text-center">
-                    <br></br>
-                    <h1>Google Map with REACT JS</h1>
-                    <hr></hr><br></br><br></br>
-                </div>
-                <br></br>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            MAPS
-            <div>
-                                <MyMapComponent
-                                    isMarkerShown
-                                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCp_difsopFyvuYa-RmUyZ_JIp66XF1ovQ&v=3.exp&libraries=geometry,drawing,places"
-                                    loadingElement={<div style={{ height: `100%` }} />}
-                                    containerElement={<div style={{ height: `400px` }} />}
-                                    mapElement={<div style={{ height: `100%` }} />}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route path="/kontak" component={Kontak} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/portofolio" component={Portofolio} />
+                </Switch>
+                <hr />
+            </BrowserRouter>
         );
     }
 }
